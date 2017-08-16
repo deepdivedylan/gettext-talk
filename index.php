f371b7daefa3941b7d3a47183dbd65e764d42de1
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-15 18:59:25 -0600

    played with var_dump when all we needed to do was restart Apache :pouting_cat:

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <?php
 $locale = filter_input(INPUT_GET, "locale", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-$supportedLocales = ["en_US", "es_MX"];
+$supportedLocales = ["en_US.utf8", "es_MX.utf8"];
 
 // set default in case of operator error
 if(in_array($locale, $supportedLocales) === false) {
-	$locale = "en_US";
+	$locale = "en_US.utf8";
 }
 
 putenv("LANG=$locale");
 setlocale(LC_ALL, $locale);
 
-bindtextdomain("gettext-talk", "locale");
-bind_textdomain_codeset("gettext-talk", "UTF-8");
+$domain = "gettext-talk";
+bindtextdomain($domain, __DIR__ . "/locale");
+bind_textdomain_codeset($domain, "UTF-8");
+textdomain($domain);
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php _("A Site About WAT"); ?></title>
+		<meta charset="UTF-8" />
+		<title><?php echo _("A Site About WAT"); ?></title>
 	</head>
 	<body>
-		<h1><?php _("WAT"); ?></h1>
-		<p><?php _("WAT is the result of {} + {} or [] + []. You can't do that! WAT!?"); ?></p>
+		<h1><?php echo _("WAT"); ?></h1>
+		<p><?php echo _("WAT is the result of {} + {} or [] + []. You can't do that! WAT!?"); ?></p>
 	</body>
 </html>
